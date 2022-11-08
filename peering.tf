31362c46b8d45f50cc1c12818bb2f46679ed5530
venkat <venkymec1922@gmail.com>
2022-11-08 16:13:46 +0530

    sample branch demo

--- peering.tf
+++ peering.tf
@@ -5,3 +5,10 @@ resource "aws_vpc_peering_connection" "management-vpc-to-env-vpc" {
   auto_accept = true
   tags        = local.vpc_peering_tags
 }
+
+resource "aws_route" "route_to_default_management_route_table" {
+  for_each                  = var.vpc
+  route_table_id            = lookup(var.management_vpc, "route_table", null)
+  destination_cidr_block    = each.value.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.management-vpc-to-env-vpc.*.id[0]
+}
