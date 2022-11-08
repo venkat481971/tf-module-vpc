a416c3b8480718f158effb0bf0679a5cc1662cc3
venkat <venkymec1922@gmail.com>
2022-11-08 16:09:42 +0530

    sample branch demo

--- subnets.tf
+++ subnets.tf
@@ -5,4 +5,6 @@ module "subnets" {
   vpc_id                    = [for k, v in aws_vpc.main : v.id]
   env                       = var.env
   subnet_availability_zones = each.value.subnet_availability_zones
+  peering_connection_id     = aws_vpc_peering_connection.management-vpc-to-env-vpc.*.id[0]
+  management_vpc            = var.management_vpc
 }
--- subnets/vars.tf
+++ subnets/vars.tf
@@ -2,3 +2,5 @@ variable "subnets" {}
 variable "vpc_id" {}
 variable "env" {}
 variable "subnet_availability_zones" {}
+variable "peering_connection_id" {}
+variable "management_vpc" {}
