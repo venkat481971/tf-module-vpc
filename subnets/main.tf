168e1d4acba27e7cd4983de245e0c33dc342c544
venkat <venkymec1922@gmail.com>
2022-11-09 14:14:46 +0530

    sample branch demo

--- subnets/main.tf
+++ subnets/main.tf
@@ -19,12 +19,12 @@ resource "aws_route_table" "aws_route_table" {
   }
 }
 
-resource "aws_route" "peering_connection_route" {
-  for_each                  = var.subnets
-  route_table_id            = lookup(lookup(aws_route_table.aws_route_table, each.value.name, null), "id", null)
-  destination_cidr_block    = lookup(var.management_vpc, "vpc_cidr", null)
-  vpc_peering_connection_id = var.peering_connection_id
-}
+#resource "aws_route" "peering_connection_route" {
+#  for_each                  = var.subnets
+#  route_table_id            = lookup(lookup(aws_route_table.aws_route_table, each.value.name, null), "id", null)
+#  destination_cidr_block    = lookup(var.management_vpc, "vpc_cidr", null)
+#  vpc_peering_connection_id = var.peering_connection_id
+#}
 
 //resource "aws_route" "gateway_connection_route" {
 //  for_each                  = var.subnets
