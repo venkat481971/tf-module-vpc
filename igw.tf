4f3834d6c21cb824baae54fa073f4a0028746f90
venkat <venkymec1922@gmail.com>
2022-11-09 10:59:50 +0530

    sample branch demo

---
+++ igw.tf
@@ -0,0 +1,12 @@
+resource "aws_internet_gateway" "gw" {
+  count  = length(local.vpc_ids)
+  vpc_id = element(local.vpc_ids, count.index)
+
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
+#
+#resource "aws_eip" "ngw" {
+#  vpc = true
+#}
