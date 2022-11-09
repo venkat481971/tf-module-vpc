64b4391efd2c26e39fc780e6c6b51ec894eefb01
venkat <venkymec1922@gmail.com>
2022-11-09 12:24:01 +0530

    sample branch demo

--- subnets.tf
+++ subnets.tf
@@ -26,9 +26,9 @@ module "private_subnets" {
 //  value = module.public_subnets
 //}
 //
-#output "private_subnets" {
-#  value = module.private_subnets
-#}
+output "private_subnets" {
+  value = module.private_subnets
+}
 
 //output "all_private_subnets" {
 //  value = [for k, v in module.private_subnets : v.subnets]
